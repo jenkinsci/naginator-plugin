2b313baee03688fcd6e831c01e051f9f4666d9e2
Tomer <tomer4@gmail.com>
2014-07-22 16:14:37 +0300

    Add authentication check for the Rebuild link

--- src/main/java/com/chikli/hudson/plugin/naginator/NaginatorRetryAction.java
+++ src/main/java/com/chikli/hudson/plugin/naginator/NaginatorRetryAction.java
@@ -2,13 +2,14 @@ package com.chikli.hudson.plugin.naginator;
 
 import hudson.model.AbstractBuild;
 import hudson.model.Action;
+import hudson.model.Item;
 import hudson.model.ParametersAction;
 import hudson.model.Run;
+import java.io.IOException;
+import jenkins.model.Jenkins;
 import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
-
-import java.io.IOException;
 
 /**
  * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
@@ -28,6 +29,7 @@ public class NaginatorRetryAction implements Action {
     }
 
     public void doIndex(StaplerResponse res, @AncestorInPath AbstractBuild build) throws IOException {
+        Jenkins.getInstance().checkPermission(Item.BUILD);
         ParametersAction p = build.getAction(ParametersAction.class);
         build.getProject().scheduleBuild(0, new NaginatorCause(), p, new NaginatorAction());
         res.sendRedirect2(build.getUpUrl());
