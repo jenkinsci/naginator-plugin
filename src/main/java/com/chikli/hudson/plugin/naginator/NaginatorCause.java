9bc90592af65aa7fcd8bcdd23aefcec707fc0111
Nowell Strite <nowell@strite.org>
2014-09-12 20:42:18 -0400

    Preserve Cause history.

--- src/main/java/com/chikli/hudson/plugin/naginator/NaginatorCause.java
+++ src/main/java/com/chikli/hudson/plugin/naginator/NaginatorCause.java
@@ -1,12 +1,16 @@
 package com.chikli.hudson.plugin.naginator;
 
-import hudson.model.Cause;
+import hudson.model.Cause.UpstreamCause;
+import hudson.model.Run;
 
 /**
  * {@link Cause} for builds triggered by this plugin.
  * @author Alan.Harder@sun.com
  */
-public class NaginatorCause extends Cause {
+public class NaginatorCause extends UpstreamCause {
+    public NaginatorCause(Run<?, ?> up) {
+        super(up);
+    }
 
     @Override
     public String getShortDescription() {
