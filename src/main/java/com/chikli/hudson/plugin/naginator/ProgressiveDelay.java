jenkinsci/naginator-plugin

commit ddaf1a4af3953846bf9573cb9926a96e8b6b0410

re-schedule limit should not consider previous failures not related to naginator

src/main/java/com/chikli/hudson/plugin/naginator/ProgressiveDelay.java
@@ -43,9 +43,11 @@ public class ProgressiveDelay extends ScheduleDelay {
         // back off at most 3 hours
 
         int n=0;
-        Run b = failedBuild;
-        for(; b!=null && b.getResult()!= Result.SUCCESS && n<max; b=b.getPreviousBuild()) {
-            n += increment;
+        Run r = failedBuild;
+        while (r != null && r.getAction(NaginatorAction.class) != null) {
+            if (n >= max) break;
+            r = r.getPreviousBuild();
+            n++;
         }
         return n;
     }

src/main/java/com/chikli/hudson/plugin/naginator/NaginatorAction.java
@@ -1,0 +1,22 @@
+package com.chikli.hudson.plugin.naginator;
+
+import hudson.model.Action;
+import hudson.model.BuildBadgeAction;
+
+/**
+ * @author: <a hef="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
+ */
+public class NaginatorAction implements BuildBadgeAction {
+
+    public String getIconFileName() {
+        return null;
+    }
+
+    public String getDisplayName() {
+        return null;
+    }
+
+    public String getUrlName() {
+        return null;
+    }
+}

src/main/java/com/chikli/hudson/plugin/naginator/NaginatorListener.java
@@ -75,7 +75,13 @@ public class NaginatorListener extends RunListener<AbstractBuild<?,?>> {
         int max = naginator.getMaxSchedule();
         if (max <=0) return true;
         int n = 0;
-        for(; r!=null && r.getResult()!= SUCCESS && n++ <= max; r=r.getPreviousBuild()) {}
+
+        while (r != null && r.getAction(NaginatorAction.class) != null) {
+            if (n >= max) break;
+            r = r.getPreviousBuild();
+            n++;
+        }
+
         return n < max;
     }
 
@@ -84,7 +90,7 @@ public class NaginatorListener extends RunListener<AbstractBuild<?,?>> {
      */
     public boolean scheduleBuild(AbstractBuild<?, ?> build, int n) {
         ParametersAction p = build.getAction(ParametersAction.class);
-        return build.getProject().scheduleBuild(n, new NaginatorCause(), p);
+        return build.getProject().scheduleBuild(n, new NaginatorCause(), p, new NaginatorAction());
     }
 
     private boolean parseLog(File logFile, String regexp) throws IOException {

src/main/resources/com/chikli/hudson/plugin/naginator/NaginatorAction/badge.jelly
@@ -1,0 +1,1 @@
+<img src="${rootUrl}/plugin/naginator/redo.png" title="${%Re-scheduled after failure}"/>

src/main/resources/com/chikli/hudson/plugin/naginator/NaginatorPublisher/config.jelly
@@ -8,16 +8,16 @@
         <f:hetero-radio descriptors="${delays}" field="delay"/>
     </f:entry>
 
-    <f:entry title="Max number of successive failed build" field="maxSchedule">
+    <f:entry title="${%Max number of successive failed build}" field="maxSchedule">
         <f:textbox/>
     </f:entry>
 
     <f:advanced>
         <f:entry field="checkRegexp"
-                 title="Only rerun build if regular expression is found in output">
+                 title="${%Only rerun build if regular expression is found in output}">
             <f:checkbox />
         </f:entry>
-        <f:entry title="Regular expression to search for" field="regexpForRerun">
+        <f:entry title="${%Regular expression to search for}" field="regexpForRerun">
             <f:textbox />
         </f:entry>
     </f:advanced>

src/main/resources/com/chikli/hudson/plugin/naginator/ProgressiveDelay/help.html
@@ -1,9 +1,11 @@
 <div xmlns="http://www.w3.org/1999/html">
-    if a build fails for a reason that cannot be immediately fixed,
+    If a build fails for a reason that cannot be immediately fixed,
     immediate rescheduling may cause a very tight loop.
     combined with publishers like e-mail, IM, this could flood the users.
     <p>
-    So to avoid this problem, progressively introduce delay until the next build
-    delay = the number of consective build problems * <em>increment</em>
-    back off at most <em>max</em>
+    So to avoid this problem, progressively introduce delay (in sec) until the next build.
+    The delay starts with the <em>increment</em> value,then it is grows by the
+    <tt>increment * number of consecutive failures</tt>
+    (e.g. starting with 5, it takes 10, 20, 35, 55 ...etc... up to the max value)
+    then the delay value remains the <em>max</em>.
 </div>

commit 7fcccbb837b8058f1f623f27061d60e6cb27e8f3

Update ProgressiveDelay.java
Updated to the newest version and progressive delay does not work anymore. Just incrementing n by 1 is not working, increment by the given increment does fix the problem. Seems to be broken with commit  ddaf1a4af3953846bf9573cb9926a96e8b6b0410

src/main/java/com/chikli/hudson/plugin/naginator/ProgressiveDelay.java
@@ -47,7 +47,7 @@ public class ProgressiveDelay extends ScheduleDelay {
         while (r != null && r.getAction(NaginatorAction.class) != null) {
             if (n >= max) break;
             r = r.getPreviousBuild();
-            n++;
+            n += increment;
         }
         return n;
     }