476d46a7a433c7a834ea03b151133a2fcf869f14
Arthur Garnier <arthur.garnier@inria.fr>
2015-05-07 16:13:18 +0200

    Regex only with MatrixPart and rerun only run where the regex is found

--- src/main/java/com/chikli/hudson/plugin/naginator/NaginatorListener.java
+++ src/main/java/com/chikli/hudson/plugin/naginator/NaginatorListener.java
@@ -23,152 +23,196 @@ import java.util.List;
 /**
  * @author <a href="mailto:nicolas.deloof@cloudbees.com">Nicolas De loof</a>
  */
-public class NaginatorListener extends RunListener<AbstractBuild<?,?>> {
+public class NaginatorListener extends RunListener<AbstractBuild<?, ?>> {
 
+	@Override
+	public void onCompleted(AbstractBuild<?, ?> build, TaskListener listener) {
+		if ((build.getResult() == SUCCESS) || (build.getResult() == ABORTED)) {
+			return;
+		}
 
-    @Override
-    public void onCompleted(AbstractBuild<?, ?> build, TaskListener listener) {
-        if ((build.getResult() == SUCCESS) || (build.getResult() == ABORTED)) {
-            return;
-        }
+		NaginatorPublisher naginator = build.getProject().getPublishersList()
+				.get(NaginatorPublisher.class);
 
-        NaginatorPublisher naginator = build.getProject().getPublishersList().get(NaginatorPublisher.class);
+		// JENKINS-13791
+		if (naginator == null) {
+			return;
+		}
 
-        // JENKINS-13791
-        if (naginator == null) {
-            return;
-        }
+		// If we're not set to rerun if unstable, and the build's unstable,
+		// return true.
+		if ((!naginator.isRerunIfUnstable())
+				&& (build.getResult() == Result.UNSTABLE)) {
+			return;
+		}
 
-        // If we're not set to rerun if unstable, and the build's unstable, return true.
-        if ((!naginator.isRerunIfUnstable()) && (build.getResult() == Result.UNSTABLE)) {
-            return;
-        }
+		// Do nothing for a single Matrix run. (Run only when all Matrix
+		// finishes)
+		if (build instanceof MatrixRun) {
+			return;
+		}
 
-        // Do nothing for a single Matrix run. (Run only when all Matrix finishes)
-        if (build instanceof MatrixRun) {
-            return;
-        }
+		// If we're supposed to check for a regular expression in the build
+		// output before
+		// scheduling a new build, do so.
+		// if (naginator.isCheckRegexp()) {
+		// LOGGER.log(Level.FINEST, "Got checkRegexp == true");
 
-        // If we're supposed to check for a regular expression in the build output before
-        // scheduling a new build, do so.
-        if (naginator.isCheckRegexp()) {
-            LOGGER.log(Level.FINEST, "Got checkRegexp == true");
+		// String regexpForRerun = naginator.getRegexpForRerun();
+		// if ((regexpForRerun !=null) && (!regexpForRerun.equals(""))) {
+		// LOGGER.log(Level.FINEST, "regexpForRerun - {0}", regexpForRerun);
 
-            String regexpForRerun = naginator.getRegexpForRerun();
-            if ((regexpForRerun !=null) && (!regexpForRerun.equals(""))) {
-                LOGGER.log(Level.FINEST, "regexpForRerun - {0}", regexpForRerun);
+		// try {
+		// // If parseLog returns false, we didn't find the regular expression,
+		// // so return true.
+		// if (!parseLog(build.getLogFile(), regexpForRerun)) {
+		// LOGGER.log(Level.FINEST, "regexp not in logfile");
+		// return;
+		// }
+		// } catch (IOException e) {
+		// e.printStackTrace(listener
+		// .error("error while parsing logs for naginator - forcing rebuild."));
+		// }
+		// }
+		// }
 
-                try {
-                    // If parseLog returns false, we didn't find the regular expression,
-                    // so return true.
-                    if (!parseLog(build.getLogFile(), regexpForRerun)) {
-                        LOGGER.log(Level.FINEST, "regexp not in logfile");
-                        return;
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace(listener
-                                      .error("error while parsing logs for naginator - forcing rebuild."));
-                }
-            }
-        }
+		LOGGER.log(Level.FINEST, "Before can Scheldule");
+		if (canSchedule(build, naginator)) {
+			int n = naginator.getDelay().computeScheduleDelay(build);
+			LOGGER.log(
+					Level.FINE,
+					"about to try to schedule a build #{0} in {1} seconds for {2}",
+					new Object[] { build.getNumber(), n,
+							build.getProject().getName() });
 
-        if (canSchedule(build, naginator)) {
-            int n = naginator.getDelay().computeScheduleDelay(build);
-            LOGGER.log(Level.FINE, "about to try to schedule a build #{0} in {1} seconds for {2}",
-                    new Object[]{build.getNumber(), n, build.getProject().getName()} );
-            
-            List<Combination> combsToRerun = new ArrayList<Combination>();
+			List<Combination> combsToRerun = new ArrayList<Combination>();
 
-            if (naginator.isRerunMatrixPart()) {
-                if (build instanceof MatrixBuild) {
-                    MatrixBuild mb = (MatrixBuild) build;
-                    List<MatrixRun> matrixRuns = mb.getRuns();
+			if (naginator.isRerunMatrixPart()) {
+				if (build instanceof MatrixBuild) {
+					MatrixBuild mb = (MatrixBuild) build;
+					List<MatrixRun> matrixRuns = mb.getRuns();
 
-                    for(MatrixRun r : matrixRuns) {
-                        if (r.getNumber() == build.getNumber()) {
-                            if ((r.getResult() == SUCCESS) || (r.getResult() == ABORTED)) {
-                                continue;
-                            }
-                            if ((!naginator.isRerunIfUnstable()) && (r.getResult() == Result.UNSTABLE)) {
-                                continue;
-                            }
-                            
-                            LOGGER.log(Level.FINE, "add combination to matrix rerun ({0})", r.getParent().getCombination().toString());
-                            combsToRerun.add(r.getParent().getCombination());    
-                        }
-                    }
+					for (MatrixRun r : matrixRuns) {
+						if (r.getNumber() == build.getNumber()) {
+							if ((r.getResult() == SUCCESS)
+									|| (r.getResult() == ABORTED)) {
+								continue;
+							}
+							if ((!naginator.isRerunIfUnstable())
+									&& (r.getResult() == Result.UNSTABLE)) {
+								continue;
+							}
+							if (naginator.isCheckRegexp()) {
+								LOGGER.log(Level.FINEST,
+										"Got checkRegexp == true");
 
-                }
-            }
+								String regexpForRerun = naginator
+										.getRegexpForRerun();
+								if ((regexpForRerun != null)
+										&& (!regexpForRerun.equals(""))) {
+									LOGGER.log(Level.FINEST,
+											"regexpForRerun - {0}",
+											regexpForRerun);
 
-            if (!combsToRerun.isEmpty()) {
-                LOGGER.log(Level.FINE, "schedule matrix rebuild");
-                scheduleMatrixBuild(build, combsToRerun, n);
-            } else {
-                scheduleBuild(build, n);
-            }
-        } else {
-            LOGGER.log(Level.FINE, "max number of schedules for #{0} build, project {1}",
-                    new Object[]{build.getNumber(), build.getProject().getName()} );
-        }
-    }
+									try {
+										if (!parseLog(r.getLogFile(),
+												regexpForRerun)) {
+											LOGGER.log(Level.FINEST,
+													"regexp not in logfile");
+											// return;
+											continue;
+										}
+									} catch (IOException e) {
+										e.printStackTrace(listener
+												.error("error while parsing logs for naginator - forcing rebuild."));
+									}
+								}
+							}
 
-    public boolean canSchedule(Run build, NaginatorPublisher naginator) {
-        Run r = build;
-        int max = naginator.getMaxSchedule();
-        if (max <=0) return true;
-        int n = 0;
+							LOGGER.log(Level.FINE,
+									"add combination to matrix rerun ({0})", r
+											.getParent().getCombination()
+											.toString());
+							combsToRerun.add(r.getParent().getCombination());
+						}
+					}
 
-        while (r != null && r.getAction(NaginatorAction.class) != null) {
-            if (n >= max) break;
-            r = r.getPreviousBuild();
-            n++;
-        }
+				}
+			}
 
-        return n < max;
-    }
+			if (!combsToRerun.isEmpty()) {
+				LOGGER.log(Level.FINE, "schedule matrix rebuild");
+				scheduleMatrixBuild(build, combsToRerun, n);
+			} else {
+				scheduleBuild(build, n);
+			}
+		} else {
+			LOGGER.log(Level.FINE,
+					"max number of schedules for #{0} build, project {1}",
+					new Object[] { build.getNumber(),
+							build.getProject().getName() });
+		}
+	}
 
-    public boolean scheduleMatrixBuild(AbstractBuild<?, ?> build, List<Combination> combinations, int n) {
-        NaginatorMatrixAction nma = new NaginatorMatrixAction();
-        for (Combination c : combinations) {
-            nma.addCombinationToRerun(c);
-        }
-        return NaginatorRetryAction.scheduleBuild(build, n, nma);
-    }
+	public boolean canSchedule(Run build, NaginatorPublisher naginator) {
+		Run r = build;
+		int max = naginator.getMaxSchedule();
+		if (max <= 0)
+			return true;
+		int n = 0;
 
-    /**
-     * Wrapper method for mocking purposes.
-     */
-    public boolean scheduleBuild(AbstractBuild<?, ?> build, int n) {
-        return NaginatorRetryAction.scheduleBuild(build, n);
-    }
+		while (r != null && r.getAction(NaginatorAction.class) != null) {
+			if (n >= max)
+				break;
+			r = r.getPreviousBuild();
+			n++;
+		}
 
-    private boolean parseLog(File logFile, String regexp) throws IOException {
+		return n < max;
+	}
 
-        if (regexp == null) {
-            return false;
-        }
+	public boolean scheduleMatrixBuild(AbstractBuild<?, ?> build,
+			List<Combination> combinations, int n) {
+		NaginatorMatrixAction nma = new NaginatorMatrixAction();
+		for (Combination c : combinations) {
+			nma.addCombinationToRerun(c);
+		}
+		return NaginatorRetryAction.scheduleBuild(build, n, nma);
+	}
 
-        // Assume default encoding and text files
-        String line;
-        Pattern pattern = Pattern.compile(regexp);
-        BufferedReader reader = null;
-        try {
-          reader = new BufferedReader(new FileReader(logFile));
-          while ((line = reader.readLine()) != null) {
-              Matcher matcher = pattern.matcher(line);
-              if (matcher.find()) {
-                  return true;
-              }
-          }
-          return false;
-        }
-        finally {
-          if(reader != null)
-            reader.close();
-        }
-    }
+	/**
+	 * Wrapper method for mocking purposes.
+	 */
+	public boolean scheduleBuild(AbstractBuild<?, ?> build, int n) {
+		return NaginatorRetryAction.scheduleBuild(build, n);
+	}
 
-    private static final Logger LOGGER = Logger.getLogger(NaginatorListener.class.getName());
+	private boolean parseLog(File logFile, String regexp) throws IOException {
+
+		if (regexp == null) {
+			return false;
+		}
+
+		// Assume default encoding and text files
+		String line;
+		Pattern pattern = Pattern.compile(regexp);
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(logFile));
+			while ((line = reader.readLine()) != null) {
+				Matcher matcher = pattern.matcher(line);
+				if (matcher.find()) {
+					return true;
+				}
+			}
+			return false;
+		} finally {
+			if (reader != null)
+				reader.close();
+		}
+	}
+
+	private static final Logger LOGGER = Logger
+			.getLogger(NaginatorListener.class.getName());
 
 }
