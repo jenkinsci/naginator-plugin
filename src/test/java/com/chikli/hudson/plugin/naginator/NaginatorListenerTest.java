4012e4cd8b6a22c9e7ff534d46d7d4058e42fd20
ikedam <devld@ikedam.jp>
2015-11-22 23:12:43 +0900

    [JENKINS-24903] Added a test to reproduce a catastrophic backtracking regular expression.

--- src/test/java/com/chikli/hudson/plugin/naginator/NaginatorListenerTest.java
+++ src/test/java/com/chikli/hudson/plugin/naginator/NaginatorListenerTest.java
@@ -235,4 +235,21 @@ public class NaginatorListenerTest extends HudsonTestCase {
                 }
         ).size());
     }
+    
+    @Bug(24903)
+    public void testCatastorophicRegularExpression() throws Exception {
+        FreeStyleProject p = createFreeStyleProject();
+        p.getBuildersList().add(new MyBuilder("0000000000000000000000000000000000000000000000000000", Result.FAILURE));
+        p.getPublishersList().add(new NaginatorPublisher(
+                "(0*)*NOSUCHSTRING",               // regexpForRerun
+                false,                  // rerunIfUnstable
+                false,                  // rerunMatrixPart
+                true,                   // checkRegexp
+                1,                      // maxSchedule
+                new FixedDelay(0)       // delay
+        ));
+        
+        p.scheduleBuild2(0);
+        waitUntilNoActivityUpTo(10 * 1000);
+    }
 }
