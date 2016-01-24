bfb39bed57e330341cf5d6ad26e6ec770cc0242e
ikedam <devld@ikedam.jp>
2016-01-24 10:57:05 +0900

    [FIXED JENKINS-21241] Provides a variable NAGINATOR_COUNT, NAGINATOR_MAXCOUNT, NAGINATOR_BUILD_NUMBER.

--- src/main/java/com/chikli/hudson/plugin/naginator/NaginatorAction.java
+++ src/main/java/com/chikli/hudson/plugin/naginator/NaginatorAction.java
@@ -1,13 +1,17 @@
 package com.chikli.hudson.plugin.naginator;
 
-import hudson.model.Action;
+import javax.annotation.CheckForNull;
+
 import hudson.model.BuildBadgeAction;
+import hudson.model.Run;
 
 /**
  * @author: <a hef="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
  */
 public class NaginatorAction implements BuildBadgeAction {
     private final int retryCount;
+    private final int maxRetryCount;
+    private final Integer parentBuildNumber;
 
     /**
      * @deprecated use {@link NaginatorAction#NaginatorAction(int)}
@@ -20,9 +24,23 @@ public class NaginatorAction implements BuildBadgeAction {
     /**
      * @param retryCount the number of retry this build is rescheduled for.
      * @since 1.16
+     * @deprecated use {@link #NaginatorAction(Run, int, int)} instead.
      */
+    @Deprecated
     public NaginatorAction(int retryCount) {
+        this(null, retryCount, 0);
+    }
+
+    /**
+     * @param parentBuild the build to retry.
+     * @param retryCount the number of retry this build is rescheduled for.
+     * @param maxRetryCount the maximum number to retry. Can be 0 for indeterminable cases.
+     * @since 1.17
+     */
+    public NaginatorAction(@CheckForNull Run<?, ?> parentBuild, int retryCount, int maxRetryCount) {
+        this.parentBuildNumber = (parentBuild != null)?parentBuild.getNumber():null;
         this.retryCount = retryCount;
+        this.maxRetryCount = maxRetryCount;
     }
 
     public String getIconFileName() {
@@ -47,5 +65,32 @@ public class NaginatorAction implements BuildBadgeAction {
      */
     public int getRetryCount() {
         return retryCount;
+    }
+
+    /**
+     * Returns the maximum number to reschedule.
+     * This may be <code>0</code> for builds rescheduled with
+     * older versions of naginator-plugin
+     * for cases that the build is rescheduled manually,
+     * or for cases the maximum number is indeterminable.
+     * 
+     * @return the maximum number to retry.
+     * @since 1.17
+     */
+    public int getMaxRetryCount() {
+        return maxRetryCount;
+    }
+
+    /**
+     * Returns the maximum number to reschedule.
+     * This may be <code>null</code> for builds rescheduled with
+     * older versions of naginator-plugin
+     * 
+     * @return the build number of the build to reschedule.
+     * @since 1.17
+     */
+    @CheckForNull
+    public Integer getParentBuildNumber() {
+        return parentBuildNumber;
     }
 }
