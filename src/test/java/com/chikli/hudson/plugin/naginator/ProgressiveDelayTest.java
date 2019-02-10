6992e35e995c9b03487ace0d6f39c04ee4887327
ikedam <devld@ikedam.jp>
2019-02-10 15:00:20 +0900

    [JENKINS-43803] Add a test to reproduce JENKINS-43803
    * ProgressiveDelay calculates wrong values when 2 series of retries run at the same time.

--- src/test/java/com/chikli/hudson/plugin/naginator/ProgressiveDelayTest.java
+++ src/test/java/com/chikli/hudson/plugin/naginator/ProgressiveDelayTest.java
@@ -1,17 +1,28 @@
 package com.chikli.hudson.plugin.naginator;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
+
+import org.junit.ClassRule;
 import org.junit.Test;
+import org.jvnet.hudson.test.FailureBuilder;
+import org.jvnet.hudson.test.Issue;
+import org.jvnet.hudson.test.JenkinsRule;
 
 import hudson.model.AbstractBuild;
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
 
 /**
  * A test suite for {@link ProgressiveDelay}.
  */
 public class ProgressiveDelayTest {
+    @ClassRule
+    public static JenkinsRule j = new JenkinsRule();
 
     @Test
     public void testComputeScheduleDelay() {
@@ -50,6 +61,49 @@ public class ProgressiveDelayTest {
         assertEquals(
                 90,
                 progressiveDelay.computeScheduleDelay(createBuild(true, createBuild(true, createBuild(false, null)))));
+    }
+
+    @Issue("JENKINS-43803")
+    @Test
+    public void testBuildsRunAlternately() throws Exception {
+        FreeStyleProject p = j.createFreeStyleProject();
+        p.getBuildersList().add(new FailureBuilder());
+
+        FreeStyleBuild buildA1 = p.scheduleBuild2(0).get();
+        assertTrue(NaginatorRetryAction.scheduleBuild(
+            buildA1,
+            0,  // delay
+            1,  // retyCount
+            120   // maxRetryCount
+        ));
+        j.waitUntilNoActivity();
+        FreeStyleBuild buildA2 = p.getLastBuild();
+
+        p.scheduleBuild2(0).get();     // buildB1
+
+        assertTrue(NaginatorRetryAction.scheduleBuild(
+            buildA2,
+            0,  // delay
+            2,  // retyCount
+            120   // maxRetryCount
+        ));
+
+        j.waitUntilNoActivity();
+        FreeStyleBuild buildA3 = p.getLastBuild();
+
+        ProgressiveDelay delay = new ProgressiveDelay(300, 0);
+        assertEquals(
+            Arrays.asList(
+                300,
+                900,
+                1800
+            ),
+            Arrays.asList(
+                delay.computeScheduleDelay(buildA1),
+                delay.computeScheduleDelay(buildA2),
+                delay.computeScheduleDelay(buildA3)
+            )
+        );
     }
 
     private static AbstractBuild createBuild(final boolean hasNaginatorAction, final AbstractBuild previousBuild) {
